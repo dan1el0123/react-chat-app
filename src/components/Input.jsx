a9af073d69afb81cfb23c86fd3442c985166b549
Daniel <conevskidaniel1999@gmail.com>
2022-09-09 16:00:01 +0200

    style chat and messages component on home page

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,20 @@
+import Img from "../img/img.png";
+import Attach from "../img/attach.png";
+
 const Input = () => {
-    return <div>Input</div>;
+    return (
+        <div className="input">
+            <input type="text" placeholder="Enter your message.." />
+            <div className="send">
+                <img src={Attach} alt="" />
+                <input id="file" type="file" style={{ display: "none" }} />
+                <label htmlFor="file">
+                    <img src={Img} alt="" />
+                </label>
+                <button>Send</button>
+            </div>
+        </div>
+    );
 };
 
 export default Input;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,22 @@
 const Message = () => {
-    return <div>Message</div>;
+    return (
+        <div className="message owner">
+            <div className="messageInfo">
+                <img
+                    src="https://thumbs.dreamstime.com/b/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg"
+                    alt="sender avatar"
+                />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>hello</p>
+                {/* <img
+                    src="https://thumbs.dreamstime.com/b/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg"
+                    alt=""
+                /> */}
+            </div>
+        </div>
+    );
 };
 
 export default Message;
