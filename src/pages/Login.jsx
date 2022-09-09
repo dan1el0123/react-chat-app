36556144a753bf616673de537e8d95e322ec72cb
Daniel <conevskidaniel1999@gmail.com>
2022-09-09 18:03:02 +0200

    add sign in with firebase and link to register on login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
 const Login = () => {
+    const [err, setErr] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/");
+        } catch (err) {
+            setErr(err.message);
+        }
+    };
+
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">Chat App</span>
-                <span className="title">Register</span>
-                <form>
+                <span className="title">Login</span>
+                <form onSubmit={handleSubmit}>
                     <input type="email" placeholder="email" />
                     <input type="password" placeholder="password" />
                     <button>Sign In</button>
+                    {err && <span>{err}</span>}
                 </form>
-                <p>You don't have an account? Register</p>
+                <p>
+                    You don't have an account?{" "}
+                    <Link to="/register">Register</Link>
+                </p>
             </div>
         </div>
     );
