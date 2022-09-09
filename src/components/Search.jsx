9514bca8fa7038682f1aace3717b56cff722bf65
Daniel <conevskidaniel1999@gmail.com>
2022-09-09 22:26:38 +0200

    add search for username and create new chat

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,107 @@
+import { useContext, useState } from "react";
+import {
+    collection,
+    query,
+    where,
+    getDocs,
+    getDoc,
+    setDoc,
+    updateDoc,
+    serverTimestamp,
+    doc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+
 const Search = () => {
+    const [username, setUsername] = useState("");
+    const [user, setUser] = useState(null);
+    const [err, setErr] = useState(false);
+
+    const { currentUser } = useContext(AuthContext);
+
+    const handleSearch = async () => {
+        const q = query(
+            collection(db, "users"),
+            where("displayName", "==", username)
+        );
+        try {
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.docs.length) {
+                setErr(true);
+                setUser(null);
+            } else {
+                querySnapshot.forEach((doc) => {
+                    setUser(doc.data());
+                });
+                setErr(false);
+            }
+        } catch (err) {
+            setErr(true);
+        }
+    };
+
+    const handleKey = (e) => {
+        e.code === "Enter" && handleSearch();
+    };
+
+    const handleSelect = async () => {
+        // check whether the pair exists(in chats in firebase), if not create
+        const combinedId =
+            currentUser.uid > user.uid
+                ? currentUser.uid + user.uid
+                : user.uid + currentUser.uid;
+
+        try {
+            const res = await getDoc(doc(db, "chats", combinedId));
+            if (!res.exists()) {
+                // create chat in chats collection for the pair of users
+                await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+                // create user chats
+                await updateDoc(doc(db, "userChats", currentUser.uid), {
+                    [combinedId + ".userInfo"]: {
+                        uid: user.uid,
+                        displayName: user.displayName,
+                        photoURL: user.photoURL,
+                    },
+                    [combinedId + ".date"]: serverTimestamp(),
+                });
+                await updateDoc(doc(db, "userChats", user.uid), {
+                    [combinedId + ".userInfo"]: {
+                        uid: currentUser.uid,
+                        displayName: currentUser.displayName,
+                        photoURL: currentUser.photoURL,
+                    },
+                    [combinedId + ".date"]: serverTimestamp(),
+                });
+            }
+        } catch (err) {}
+
+        setUser(null);
+        setUsername("");
+    };
+
     return (
         <div className="search">
             <div className="searchForm">
-                <input type="text" placeholder="Find a user" />
-            </div>
-            <div className="userChat">
-                <img
-                    src="https://thumbs.dreamstime.com/b/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg"
-                    alt="User avatar"
+                <input
+                    type="text"
+                    placeholder="Find a user"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    onKeyDown={handleKey}
                 />
-                <div className="userChatInfo">
-                    <span>Jane</span>
-                </div>
             </div>
+            {err && <span>User not found!</span>}
+            {user && (
+                <div className="userChat" onClick={handleSelect}>
+                    <img src={user.photoURL} alt="User avatar" />
+                    <div className="userChatInfo">
+                        <span>{user.displayName}</span>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
