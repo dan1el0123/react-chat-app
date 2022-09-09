ca3da1821d76a9e35414331be563b923f26290a2
Daniel <conevskidaniel1999@gmail.com>
2022-09-09 17:04:41 +0200

    initial react router paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import "./style.scss";
 
 function App() {
     return (
-        <div>
-            <Home />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
