800cfe8af7a64cbe29bceca62c33a9323f473008
Daniel <conevskidaniel1999@gmail.com>
2022-09-09 18:22:38 +0200

    display logged user info in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import { signOut } from "firebase/auth";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { auth } from "../firebase";
 
 const Navbar = () => {
+    const { currentUser } = useContext(AuthContext);
+
     return (
         <div className="navbar">
             <span className="logo">Chat App</span>
             <div className="user">
-                <img
-                    src="https://thumbs.dreamstime.com/b/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg"
-                    alt="User Avatar"
-                />
-                <span>John</span>
+                <img src={currentUser.photoURL} alt="User Avatar" />
+                <span>{currentUser.displayName}</span>
                 <button onClick={() => signOut(auth)}>Logout</button>
             </div>
         </div>
